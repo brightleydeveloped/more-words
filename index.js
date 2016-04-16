a7c9a4787ffcf0ddcbbcc15c6e902dbfa340198b
Pat Lillis <lillispm@gmail.com>
2016-04-16 10:38:55 -0400

    fix export

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _words = require('./words.json');
+
+var _words2 = _interopRequireDefault(_words);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _words2.default;
+//module.exports = words.words;
+//words.words.lengths = words.lengths;
+
+module.exports = exports['default'];
